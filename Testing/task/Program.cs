49af9000e467f281a3cc25fff96ee03e2c3efb57
LobovaElizaveta <116665647+LobovaElizaveta@users.noreply.github.com>
2022-12-28 02:16:32 +0300

    Add files via upload

---
+++ Testing/task/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] massive ={"1", "bb3", "Russia", "it", "w3e4"};
+
+var result = new string[massive.Length];
+var length = 0;
+foreach(var element in massive)
+{
+    if( element.Length <=2)
+    {
+        result[length]=element;
+        length++;
+    }
+}
+Console.WriteLine(String.Join(" ", result));
